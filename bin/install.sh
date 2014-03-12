9c71066e239375fa6213c34e1c0610b94473e946
Adam Lukens <alukens@purduefed.com>
2014-03-12 00:38:27 -0400

    Add a script to install dotfiles on a new machine

---
+++ bin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+git clone git@github.com:McPolemic/dotfiles.git
+cd dotfiles
+
+# Set globs to show dotfiles
+shopt -s dotglob
+for d in *; do ln -s `pwd`/${d} ~/${d}; done
+# Set it back to normal
+shopt -u dotglob
