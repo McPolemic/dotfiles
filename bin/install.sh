828ae65351568903d06e5d7bdcff486f32378112
Adam Lukens <adam@testdouble.com>
2019-06-12 13:31:35 -0400

    Modify install script to handle new computers
    Namely to set up SSH keys and allow adding them to a GitHub account

--- bin/install.sh
+++ bin/install.sh
@@ -2,19 +2,37 @@
 set -e
 
 # Editable
-#git clone --recursive git@github.com:McPolemic/dotfiles.git
+git clone --recursive git@github.com:McPolemic/dotfiles.git
 
 # Read-only
-git clone --recursive https://github.com/McPolemic/dotfiles.git
+# git clone --recursive https://github.com/McPolemic/dotfiles.git
 cd dotfiles
+
+# Create an SSH key if needed
+if [ ! -d ~/.ssh ]
+then
+  echo "No SSH key found. Creating a new one..."
+  ssh-keygen -f ~/.ssh/id_rsa
+
+  echo "Public key (for adding to GitHub)"
+  echo "================================="
+  cat ~/.ssh/id_rsa
+  echo "================================="
+  echo
+  echo "Press enter to continue..."
+  # Pause for copying
+  read
+fi
 
 # Set globs to show dotfiles
 shopt -s dotglob
+
 for d in *
   do if [ -e ~/${d} ]
        then mv ~/${d} ~/${d}.bak
      fi
      ln -s `pwd`/${d} ~/${d}
 done
+
 # Set it back to normal
 shopt -u dotglob
